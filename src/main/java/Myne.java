c00705ed95441cbc3bd348cd9b59c571272c9b00
SpiritSK <devonsebastiankoswara@gmail.com>
2026-01-15 23:43:32 +0800

    Myne will echo user text and exit on 'bye'

--- src/main/java/Myne.java
+++ src/main/java/Myne.java
@@ -1,6 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Myne {
     public static void main(String[] args) {
         greet();
+
+        String exitMessage = "Farewell. May the time come when our threads of fate are woven together again.";
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println("_____________________________________________");
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(exitMessage);
+                System.out.println("_____________________________________________");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("_____________________________________________");
+            }
+        }
     }
 
     private static void greet() {
@@ -15,12 +33,8 @@ public class Myne {
                 + "        |___/";
         String greetingEnd = "May our meeting, ordained by the gods be blessed on this fruitful day.";
 
-        String exitMessage = "Farewell. May the time come when our threads of fate are woven together again.";
-
         System.out.println("_____________________________________________");
         System.out.println(greetingStart + "\n" + logo + "\n" + greetingEnd);
-        System.out.println("_____________________________________________");
-        System.out.println(exitMessage);
         System.out.println("_____________________________________________");
     }
 }

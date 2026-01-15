d2d846d71991f854775843d4d61857457f77075c
SpiritSK <devonsebastiankoswara@gmail.com>
2026-01-15 23:34:03 +0800

    Moved the greeting into its own function

--- src/main/java/Myne.java
+++ src/main/java/Myne.java
@@ -1,14 +1,18 @@
 public class Myne {
     public static void main(String[] args) {
+        greet();
+    }
+
+    private static void greet() {
         String greetingStart = "Good day to you. My name is";
         String logo = "___  ___\n"
-                    + "|  \\/  |                 \n"
-                    + "| .  . |_   _ _ __   ___\n"
-                    + "| |\\/| | | | | '_ \\ / _ \\\n"
-                    + "| |  | | |_| | | | |  __/\n"
-                    + "\\_|  |_/\\__, |_| |_|\\___|\n"
-                    + "         __/ |\n"
-                    + "        |___/";
+                + "|  \\/  |                 \n"
+                + "| .  . |_   _ _ __   ___\n"
+                + "| |\\/| | | | | '_ \\ / _ \\\n"
+                + "| |  | | |_| | | | |  __/\n"
+                + "\\_|  |_/\\__, |_| |_|\\___|\n"
+                + "         __/ |\n"
+                + "        |___/";
         String greetingEnd = "May our meeting, ordained by the gods be blessed on this fruitful day.";
 
         String exitMessage = "Farewell. May the time come when our threads of fate are woven together again.";

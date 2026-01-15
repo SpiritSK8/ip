9a4fd9bcd29acf75b3cf04f25f73a005b18b5bc7
SpiritSK <devonsebastiankoswara@gmail.com>
2026-01-15 23:29:43 +0800

    Added greeting and farewell messages

--- src/main/java/Myne.java
+++ src/main/java/Myne.java
@@ -1,5 +1,6 @@
 public class Myne {
     public static void main(String[] args) {
+        String greetingStart = "Good day to you. My name is";
         String logo = "___  ___\n"
                     + "|  \\/  |                 \n"
                     + "| .  . |_   _ _ __   ___\n"
@@ -8,7 +9,14 @@ public class Myne {
                     + "\\_|  |_/\\__, |_| |_|\\___|\n"
                     + "         __/ |\n"
                     + "        |___/";
+        String greetingEnd = "May our meeting, ordained by the gods be blessed on this fruitful day.";
 
-                System.out.println("Hello from\n" + logo);
+        String exitMessage = "Farewell. May the time come when our threads of fate are woven together again.";
+
+        System.out.println("_____________________________________________");
+        System.out.println(greetingStart + "\n" + logo + "\n" + greetingEnd);
+        System.out.println("_____________________________________________");
+        System.out.println(exitMessage);
+        System.out.println("_____________________________________________");
     }
 }

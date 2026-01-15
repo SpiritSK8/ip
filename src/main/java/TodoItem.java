6abc79f72b09900374f9f9934edcd2cfbfcf464a
SpiritSK <devonsebastiankoswara@gmail.com>
2026-01-16 00:41:05 +0800

    Added mark and unmark feature

--- src/main/java/TodoItem.java
+++ src/main/java/TodoItem.java
@@ -1,3 +1,17 @@
 public class TodoItem {
+    private String name;
+    private boolean isDone;
 
+    public TodoItem(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
 }

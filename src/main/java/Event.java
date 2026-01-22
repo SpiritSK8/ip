82e4f9b19fd3419af6c2fc34c188a15c7425208b
SpiritSK <devonsebastiankoswara@gmail.com>
2026-01-23 01:38:18 +0900

    Added Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,19 @@
+public class Event extends Task{
+    private String startDate;
+    private String endDate;
+
+    public Event(String name, String startDate, String endDate) {
+        super(name);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private String getTypeIcon() {
+        return "[E]";
+    }
+
+    @Override
+    public String toString() {
+        return getTypeIcon() + super.toString() + "(from: " + startDate + " to: " + endDate + ")";
+    }
+}

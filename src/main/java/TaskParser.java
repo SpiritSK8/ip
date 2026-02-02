e8540c4fbf796f46236fc5a7c7847bcd08a63a39
SpiritSK <devonsebastiankoswara@gmail.com>
2026-02-02 18:26:54 +0800

    Change to static method

--- src/main/java/TaskParser.java
+++ src/main/java/TaskParser.java
@@ -2,13 +2,8 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class TaskParser {
-    private File taskFile;
-
-    public TaskParser() {
-        this.taskFile = TaskFile.fetchTasks();
-    }
-
-    public ArrayList<Task> parseTaskFile() {
+    public static ArrayList<Task> parseTaskFile() {
+        File taskFile = TaskFile.fetchTasks();
         return null;
     }
 }

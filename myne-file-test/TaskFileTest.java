5be0649f58bcda6f67972269b59a4eef84d72db1
SpiritSK <devonsebastiankoswara@gmail.com>
2026-02-02 18:24:54 +0800

    Add testcases for task file fetching

---
+++ myne-file-test/TaskFileTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskFileTest {
+    @Test
+    public void fetchTasksTest() {
+        File taskFile = TaskFile.fetchTasks();
+        assertEquals(new File("./data/myne.txt"), taskFile);
+    }
+}

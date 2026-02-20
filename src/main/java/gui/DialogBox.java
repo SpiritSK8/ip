b49d05914a5bac036a89560d9318b655c1e32e88
SpiritSK <devonsebastiankoswara@gmail.com>
2026-02-21 03:46:52 +0800

    Add dynamic margin for chat bubble

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -7,12 +7,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
 /**
@@ -20,6 +22,14 @@ import javafx.scene.shape.Circle;
  * and a label containing text from the speaker.
  */
 public class DialogBox extends HBox {
+    /**
+     * Chat bubble margin for the opposite side.
+     * For example, the user's chat bubbles are on the left, so extra margin on the right is needed.
+     */
+    private static final double oppositeMargin = 80.0;
+
+    @FXML
+    private VBox dialogBubble;
     @FXML
     private Label dialog;
     @FXML
@@ -41,19 +51,30 @@ public class DialogBox extends HBox {
 
     @FXML
     public void initialize() {
+        // Clips the profile image into a circle.
         double radius = Math.min(displayPicture.getFitWidth(), displayPicture.getFitHeight()) / 2;
         Circle clip = new Circle(radius, radius, radius);
         displayPicture.setClip(clip);
+
+        // Sets the margin.
+        setPadding(new Insets(5.0, 5.0, 5.0, oppositeMargin));
     }
 
     /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     * Flips the dialog box for messages sent by the user.
      */
     private void flip() {
+        // Flips the order of elements (i.e. from (label, image) to (image, label)).
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
+
         setAlignment(Pos.TOP_LEFT);
+
+        dialogBubble.getStyleClass().add("reply-dialog-bubble");
+
+        // Taken from ChatGPT: https://chatgpt.com/share/6998b63e-85a8-800d-a1e3-7d7031147a98
+        setPadding(new Insets(5.0, oppositeMargin, 5.0, 5.0)); // More right margin
     }
 
     public static DialogBox getUserDialog(String text, Image img) {

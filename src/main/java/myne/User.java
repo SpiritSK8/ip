813d91bfc19328230190c7de411b30a52a7e1776
SpiritSK <devonsebastiankoswara@gmail.com>
2026-02-23 06:27:54 +0800

    Change to record class

--- src/main/java/myne/User.java
+++ src/main/java/myne/User.java
@@ -5,23 +5,7 @@ import javafx.scene.paint.Color;
 /**
  * A class to encapsulate a username and its color.
  */
-public class User {
-    public static final User MYNE = new User("Myne", Color.rgb(110, 84, 5));
+public record User(String name, Color color) {
+    public static final User MYNE = new User("Myne", Color.rgb(173, 132, 7));
     public static final User FERDINAND = new User("Ferdinand", Color.rgb(2, 61, 129));
-
-    private final String name;
-    private final Color color;
-
-    public User(String name, Color color) {
-        this.name = name;
-        this.color = color;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Color getColor() {
-        return color;
-    }
 }
